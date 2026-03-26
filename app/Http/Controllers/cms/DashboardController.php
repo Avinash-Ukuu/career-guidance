50b80060d2032a5646d03f72b27f7aa9a52eb2b3
Avinash <avinash.ukuu@gmail.com>
2026-03-26 13:49:29 +0530

    modification in dashboard

--- app/Http/Controllers/cms/DashboardController.php
+++ app/Http/Controllers/cms/DashboardController.php
@@ -7,18 +7,59 @@ use App\Models\Career;
 use App\Models\Skill;
 use App\Models\StudentAnswer;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        $totalStudents  = User::where('role','student')->count();
-        $totalCareers   = Career::count();
-        $totalSkills    = Skill::count();
+        if(auth()->user()->role == 'admin')
+        {
+            $totalStudents  = User::where('role','student')->count();
+            $totalCareers   = Career::count();
+            $totalSkills    = Skill::count();
 
-        $totalTests     = StudentAnswer::distinct('student_id')->count('student_id');
+            $totalTests     = StudentAnswer::distinct('student_id')->count('student_id');
 
-        return view('cms.dashboard', compact('totalStudents', 'totalCareers', 'totalSkills', 'totalTests'));
+            return view('cms.dashboard', compact('totalStudents', 'totalCareers', 'totalSkills', 'totalTests'));
+        }else{
+            $student        =   auth()->user()->student;
+            $answers        =   StudentAnswer::where('student_id', $student->id)->with('question')->get();
+
+            $skillScores    =   [];
+
+            foreach ($answers as $ans) {
+                $skill_id = $ans->question->skill_id;
+
+                if (!isset($skillScores[$skill_id])) {
+                    $skillScores[$skill_id] = 0;
+                }
+
+                $skillScores[$skill_id] += $ans->score;
+            }
+
+            $topSkill = null;
+            $career = null;
+
+            if (!empty($skillScores)) {
+                $topScore = max($skillScores);
+                $topSkillId = array_search($topScore, $skillScores);
+
+                $topSkill = Skill::find($topSkillId);
+
+                $career = Career::whereHas('skills', function ($q) use ($topSkillId) {
+                    $q->where('skills.id', $topSkillId);
+                })->first();
+            }
+
+            $totalTests = StudentAnswer::where('student_id', $student->id)->count();
+
+            return view('cms.dashboard', compact(
+                'student',
+                'totalTests',
+                'topSkill',
+                'career'
+            ));
+        }
+
     }
 }
--- resources/views/cms/dashboard.blade.php
+++ resources/views/cms/dashboard.blade.php
@@ -1,63 +1,132 @@
 @extends('cms.layouts.master')
 @section('content')
-    <div class="col-12 mt-5">
-        <div class="row">
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3>{{ $totalStudents }}</h3>
-                        <p>Total Students</p>
+
+    @if (auth()->user()->role == 'admin')
+        <div class="col-12 mt-5">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $totalStudents }}</h3>
+                            <p>Total Students</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-users"></i>
+                        </div>
+                        {{-- <a href="{{ route('cms.user.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-users"></i>
-                    </div>
-                    {{-- <a href="{{ route('cms.user.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>{{ $totalCareers }}</h3>
-                        <p>Total Careers</p>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $totalCareers }}</h3>
+                            <p>Total Careers</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user-graduate"></i>
+                        </div>
+                        {{-- <a href="{{ route('cms.career.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-user-graduate"></i>
-                    </div>
-                    {{-- <a href="{{ route('cms.career.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3>{{ $totalSkills }}</h3>
-                        <p>Total Skills</p>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $totalSkills }}</h3>
+                            <p>Total Skills</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-lightbulb"></i>
+                        </div>
+                        {{-- <a href="{{ route('cms.skill.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-lightbulb"></i>
-                    </div>
-                    {{-- <a href="{{ route('cms.skill.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>{{ $totalTests }}</h3>
-                        <p>Tests Taken</p>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $totalTests }}</h3>
+                            <p>Tests Taken</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-book-reader"></i>
+                        </div>
+                        {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-book-reader"></i>
-                    </div>
-                    {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                 </div>
+                <!-- ./col -->
             </div>
-            <!-- ./col -->
         </div>
-    </div>
+    @else
+        <div class="container">
+
+            <h2 class="mb-4"> Welcome, {{ auth()->user()->name }}</h2>
+
+            <div class="row">
+
+                {{-- Tests Attempted --}}
+                <div class="col-md-4">
+                    <div class="card bg-primary text-white p-3">
+                        <h4>{{ $totalTests ?? 0 }}</h4>
+                        <p>Tests Attempted</p>
+                    </div>
+                </div>
+
+                {{-- Top Skill --}}
+                <div class="col-md-4">
+                    <div class="card bg-success text-white p-3">
+                        <h4>{{ $topSkill->name ?? 'Not Available' }}</h4>
+                        <p>Your Strongest Skill</p>
+                    </div>
+                </div>
+
+                {{-- Recommended Career --}}
+                <div class="col-md-4">
+                    <div class="card bg-warning text-white p-3">
+                        <h4>{{ $career->name ?? 'Not Available' }}</h4>
+                        <p>Recommended Career</p>
+                    </div>
+                </div>
+
+            </div>
+
+            {{-- Career Details --}}
+            @if ($career)
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h4>Career Details</h4>
+                    </div>
+
+                    <div class="card-body">
+                        <h5>{{ $career->name }}</h5>
+                        <p>{{ $career->description }}</p>
+                    </div>
+                </div>
+            @endif
+
+            <div class="card mt-4">
+                <div class="card-header">
+                    <h4>Quick Actions</h4>
+                </div>
+
+                <div class="card-body text-center">
+
+                    <a href="{{ route('cms.careerTest') }}" class="btn btn-primary m-2">
+                        Take Career Test
+                    </a>
+
+                    <a href="{{ route('cms.careerResult') }}" class="btn btn-success m-2">
+                        View Result
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+    @endif
 @endsection
